8a7146019073ba72566c46dbf5947505cfd3859d
pavik0413 <pavithrakcse@gmail.com>
2024-05-08 16:59:25 +0530

    first commit

---
+++ app/src/main/java/com/example/moody/ProfileActivity.java
@@ -0,0 +1,21 @@
+package com.example.moody;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+import java.util.List;
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+    }
+}
